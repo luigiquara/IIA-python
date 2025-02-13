0554df31af99480d171a2eb8e24d373d3ac2d6f4
luigiquara <quarantiello.luigi0@gmail.com>
2025-02-13 15:59:15 +0100

    variabili in italiano

--- src/local_search.py
+++ src/local_search.py
@@ -4,25 +4,25 @@ import sys
 
 from utils import Node
 
-def hill_climbing(problem):
+def hill_climbing(problema):
     # Ricerca locale hill climbing
-    current = Node(problem.initial_state)
+    nodo_corrente = Node(problema.initial_state)
 
     while True:
         # Il metodo expand restituisce tutti i vicini di un nodo, uno alla volta
         # In questo modo, si ottengono tutti i nodi vicini
-        neighbors = list(current.expand(problem))
-        if not neighbors: # se il nodo corrente non ha vicini interrompi e restituisci current
+        vicini = list(nodo_corrente.expand(problema))
+        if not vicini: # se il nodo corrente non ha vicini interrompi e restituisci current
             break
 
         # Seleziona il vicino con il valore più alto
-        highest_neighbor = sorted(neighbors, key=lambda x: problem.value(x), reverse=True)[0]
+        vicino_maggiore = sorted(vicini, key=lambda x: problema.value(x), reverse=True)[0]
         # Se il vicino maggiore ha comunque un valore minore del nodo corrente, interrompe l'esecuzione
-        if problem.value(highest_neighbor) <= problem.value(current):
+        if problema.value(vicino_maggiore) <= problema.value(nodo_corrente):
             break
         else:
-            current = highest_neighbor
-    return current
+            nodo_corrente = vicino_maggiore
+    return nodo_corrente
 
 def exp_schedule(k=100, lam=0.5, limit=100):
     # Una possibile funzione di scheduling per simulated annealing.
@@ -30,22 +30,22 @@ def exp_schedule(k=100, lam=0.5, limit=100):
     # k indica la temperatura iniziale
     return lambda t: (k * math.exp(-lam * t) if t < limit else 0)
 
-def simulated_annealing(problem, schedule=exp_schedule()):
+def simulated_annealing(problema, schedule=exp_schedule()):
     # Ricerca locale simulated annealing
     # L'algoritmo richiede una funzione di scheduling
     # per determinare l'andamento della temperatura
-    current = Node(problem.initial_state)
+    nodo_corrente = Node(problema.initial_state)
 
     for t in range(sys.maxsize): #sys.maxsize è il massimo numero intero
         temp = schedule(t)
-        if temp == 0: return current
+        if temp == 0: return nodo_corrente
 
-        neighbors = list(current.expand(problem))
-        if not neighbors: # se il nodo corrente non ha vicini interrompi e restituisci current
+        vicini = list(nodo_corrente.expand(problema))
+        if not vicini: # se il nodo corrente non ha vicini interrompi e restituisci current
             break
 
         # il nodo successivo è scelto casualmente tra i vicini di quello corrente
-        next = random.choice(neighbors)
-        delta_e = problem.value(next) - problem.value(current)
+        prossimo_nodo = random.choice(vicini)
+        delta_e = problema.value(prossimo_nodo) - problema.value(nodo_corrente)
         if delta_e > 0 or random.random()<(math.exp(delta_e / temp)):
-            current = next
+            nodo_corrente = prossimo_nodo
--- src/search.py
+++ src/search.py
@@ -9,155 +9,155 @@ def best_first_search(problem, f):
     # ricerca best_first su grafo
     # la ricerca best_first utilizza una coda con priorità come frontiera
     # la coda viene inizializzata inserendo il primo nodo del problema
-    node = Node(problem.initial_state)
-    frontier = PriorityQueue(f=f)
-    frontier.insert(node)
-    explored = {problem.initial_state: node} # insieme degli stati già visitati
+    nodo = Node(problem.initial_state)
+    frontiera = PriorityQueue(f=f)
+    frontiera.insert(node)
+    raggiunti = {problem.initial_state: nodo} # insieme degli stati già visitati
 
-    while frontier: # finchè ci sono nodi nella frontiera
+    while frontiera: # finchè ci sono nodi nella frontiera
         # seleziona il nodo per l'espansione
-        node = frontier.pop()
+        nodo = frontiera.pop()
         # controlla se il nodo è uno stato obiettivo
-        if problem.is_goal(node.state): return node
+        if problema.is_goal(nodo.state): return nodo
 
         # se non è uno stato obiettivo, il nodo viene espanso per aggiornare la frontiera
-        for child in node.expand(problem):
-            s = child.state
+        for nodo_figlio in nodo.expand(problema):
+            s = nodo_figlio.state
             # lo stato s viene aggiunto alla frontiera se:
             # 1. non fa parte dell'insieme dei nodi esplorati
             # 2. fa parte dei nodi già esplorati, ma ora è stato raggiunto seguendo un cammino di costo minore
-            if s not in explored or child.path_cost < explored[s].path_cost:
-                explored[s] = child
-                frontier.insert(child)
+            if s not in raggiunti or nodo_figlio.path_cost < raggiunti[s].path_cost:
+                raggiunti[s] = nodo_figlio
+                frontiera.insert(nodo_figlio)
     return None # in questo caso ritorna fallimento
 
 # Restituisce True se il nodo 'node' forma un cammino ciclico di lunghezza k o minore
-def is_cycle(node, k=30):
-    ancestor = node.parent
+def is_cycle(nodo, k=30):
+    nodo_padre = node.parent
     steps = 0
 
-    while ancestor is not None and steps < k:
-        if ancestor.state == node.state:
+    while nodo_padre is not None and steps < k:
+        if nodo_padre.state == nodo.state:
             return True  # Abbiamo trovato un ciclo
-        ancestor = ancestor.parent
+        nodo_padre = nodo_padre.parent
         steps += 1
 
     return False
 
-def best_first_search_tree(problem, f):
+def best_first_search_tree(problema, f):
     # ricerca best-first su albero - non mantiene la collezione 'explored'
     # la ricerca best_first utilizza una coda con priorità come frontiera
     # la coda viene inizializzata inserendo il primo nodo del problema
-    node = Node(problem.initial_state)
-    frontier = PriorityQueue(f=f)
-    frontier.insert(node)
+    nodo = Node(problema.initial_state)
+    frontiera = PriorityQueue(f=f)
+    frontiera.insert(node)
 
-    while frontier: # finchè ci sono nodi nella frontiera
+    while frontiera: # finchè ci sono nodi nella frontiera
         # seleziona il nodo per l'espansione
-        node = frontier.pop()
+        nodo = frontiera.pop()
         # controlla se il nodo è uno stato obiettivo
-        if problem.is_goal(node.state):
-            return node
+        if problema.is_goal(node.state):
+            return nodo
 
         # se non è uno stato obiettivo, il nodo viene espando per aggiornare la frontiera
-        for child in node.expand(problem):
+        for nodo_figlio in nodo.expand(problema):
             # non vengono aggiunti alla frontiera i nodi che formano cammini ciclici
             #if not is_cycle(child):
                 #frontier.insert(child)
-            frontier.insert(child)
+            frontiera.insert(nodo_figlio)
     return None # in questo caso ritorna fallimento
 
-def breadth_first_search(problem):
+def breadth_first_search(problema):
     # Ricerca-grafo in ampiezza
     # La funzione di valutazione è la lunghezza -> vengono esaminati prima i nodi a profondità minore
-    return best_first_search(problem, f=len)
+    return best_first_search(problema, f=len)
 
-def depth_first_search(problem):
+def depth_first_search(problema):
     # Ricerca-grafo in profondità
     # La funzione di valutazione è l'opposto della lunghezza -> vengono esaminati prima i nodi a profondità maggiore
-    return best_first_search(problem, f=lambda n: -len(n))
+    return best_first_search(problema, f=lambda n: -len(n))
 
-def depth_first_search_tree(problem):
-    return best_first_search_tree(problem, f=lambda n: -len(n))
+def depth_first_search_tree(problema):
+    return best_first_search_tree(problema, f=lambda n: -len(n))
 
-def depth_first_recursive_search(problem, node=None):
+def depth_first_recursive_search(problema, nodo=None):
     # Ricerca in profondità ricorsiva
-    if node is None:
-        node = Node(problem.initial_state)
+    if nodo is None:
+        nodo = Node(problema.initial_state)
 
     # controlla se lo stato del nodo è uno stato obiettivo
-    if problem.is_goal(node.state):
-        return node
+    if problema.is_goal(nodo.state):
+        return nodo
     # controlla se si è in un cammino ciclico
-    elif is_cycle(node):
+    elif is_cycle(nodo):
         return None
     
     # se il nodo obiettivo non è stato trovato
     # ripeti la ricerca espandendo l'albero di ricerca
     else:
-        for child_node in node.expand(problem):
-            result = depth_first_recursive_search(problem, child_node)
+        for nodo_figlio in nodo.expand(problema):
+            result = depth_first_recursive_search(problema, nodo_figlio)
             if result:
                 return result
 
-def depth_limited_search(problem, depth_limit):
-    """Ricerca in profondità con depth_limit"""
-    frontier = LIFOQueue([Node(problem.initial_state)])
+def depth_limited_search(problema, depth_limit):
+    """Ricerca a profondità limitata"""
+    frontiera = LIFOQueue([Node(problem.initial_state)])
 
     # finchè la frontiera non è vuota
-    while frontier:
-        node = frontier.pop() # estrae un nodo dalla frontiera
+    while frontiera:
+        nodo = frontiera.pop() # estrae un nodo dalla frontiera
         # controlla se lo stato del nodo è uno stato obiettivo
-        if problem.is_goal(node.state):
-            return node
+        if problema.is_goal(nodo.state):
+            return nodo
         # controlla se si è superato il limite di profondità
-        elif len(node) > depth_limit:
+        elif len(nodo) > depth_limit:
             continue
             #return None
         # controlla se si è in un cammino ciclico
-        elif is_cycle(node):
+        elif is_cycle(nodo):
             continue
 
         # altrimenti, espandi la frontiera
         else:
-            for child in node.expand(problem):
-                frontier.append(child)
+            for nodo_figlio in nodo.expand(problema):
+                frontiera.append(nodo_figlio)
 
     return None # in questo caso ritorna con fallimento
 
-def depth_recursive_limited_search(problem, node=None, depth_limit=100):
-    """Ricerca in profondità ricorsiva con depth_limit"""
+def depth_recursive_limited_search(problema, nodo=None, depth_limit=100):
+    """Ricerca a profondità limitata ricorsiva"""
 
-    if node is None:
-        node = Node(problem.initial_state)
+    if nodo is None:
+        nodo = Node(problem.initial_state)
     # controllo per fermare la ricorsione
     if depth_limit < 0:
         return None
     # controlla se lo stato del nodo è uno stato obiettivo
-    if problem.is_goal(node.state):
-        return node
+    if problema.is_goal(nodo.state):
+        return nodo
     # controlla se si è in cammino ciclico
-    if is_cycle(node):
+    if is_cycle(nodo):
         return None
 
     # altrimenti, continua la ricerca
-    for child_node in node.expand(problem):
-        result = depth_recursive_limited_search(problem, child_node, depth_limit-1)
+    for nodo_figlio in nodo.expand(problema):
+        result = depth_recursive_limited_search(problema, nodo_figlio, depth_limit-1)
         if result:
             return result
 
 # funzione di valutazione per la ricerca uniform_cost
 def g(n): return n.path_cost
 
-def uniform_cost_search(problem):
+def uniform_cost_search(problema):
     # Ricerca-grafo a costo uniforme
     # La funzione di valutazione dipende dal path_cost -> vengono esaminati prima i nodi a costo minore
-    return best_first_search(problem, f=g)
+    return best_first_search(problema, f=g)
 
-def astar_search(problem, h=None):
+def astar_search(problema, h=None):
     # Ricerca A*
     # È possibile definire una funzione euristica custom,
     # altrimenti viene utilizzata l'euristica indicata nella definizione del problema
-    h = h or problem.h
+    h = h or problema.h
     # vengono esaminati prima i nodi a costo stimato minore, seguendo la funzione di valutazione di A*
-    return best_first_search(problem, f=lambda n: g(n) + h(n))
+    return best_first_search(problema, f=lambda n: g(n) + h(n))
