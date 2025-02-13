251bdefff2bf8f7655cf508a4b23044bd1e94836
luigiquara <quarantiello.luigi0@gmail.com>
2025-02-13 09:12:25 +0100

    refactor

--- src/local_search.py
+++ src/local_search.py
@@ -21,6 +21,12 @@ def hill_climbing(problem):
             current = highest_neighbor
     return current
 
+def exp_schedule(k=100, lam=0.5, limit=100):
+    # Una possibile funzione di scheduling per simulated annealing.
+    # In questo caso, la temperatura decresce seguendo una legge esponenziale
+    # k indica la temperatura iniziale
+    return lambda t: (k * math.exp(-lam * t) if t < limit else 0)
+
 def simulated_annealing(problem, schedule=exp_schedule()):
     # Ricerca locale simulated annealing
     # L'algoritmo richiede una funzione di scheduling
@@ -40,9 +46,3 @@ def simulated_annealing(problem, schedule=exp_schedule()):
         delta_e = next.state - current.state
         if delta_e > 0 or random.random()<(math.exp(delta_e / temp)):
             current = next
-
-def exp_schedule(k=100, lam=0.5, limit=100):
-    # Una possibile funzione di scheduling per simulated annealing.
-    # In questo caso, la temperatura decresce seguendo una legge esponenziale
-    # k indica la temperatura iniziale
-    return lambda t: (k * math.exp(-lam * t) if t < limit else 0)
